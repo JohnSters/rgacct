7e4986561c141279d477ba950a8de69a1ea258b5
jonathan <jonnyschoeman@gmail.com>
2022-11-17 09:16:53 +0200

    First Commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {}
+  }
+}
+</script>
