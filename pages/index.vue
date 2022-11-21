b6aed646599fe2ad99f38776edeb8a600be8da89
jonathan <jonnyschoeman@gmail.com>
2022-11-21 13:32:40 +0200

    Added some more changes to mobile

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <v-container>
-    <v-card class="ma-auto text-center" height="400" flat>
+    <v-card class="ma-auto hidden-sm-and-down text-center" height="400" flat>
       <v-img src="/logo/logo.svg" height="400" contain></v-img>
+    </v-card>
+    <v-card class="ma-auto hidden-md-and-up text-center" height="200" flat>
+      <v-img src="/logo/logo.svg" height="200" contain></v-img>
+      <v-img src="/plant.png" class="ml-n8 hidden-md-and-up" style="margin-bottom: 55px" width="150" contain></v-img>
     </v-card>
   </v-container>
 </template>
